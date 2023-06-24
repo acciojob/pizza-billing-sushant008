732d165a792a74b1b85d19aea0f285589d55eace
sushant008 <129641196+sushant008@users.noreply.github.com>
2023-06-24 10:08:56 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,9 @@
 package com.driver;
 
 public class Main {
+  public Main() {
+  }
+
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
     System.out.println(dp.getPrice());
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,29 +6,95 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
-    public Pizza(Boolean isVeg){
+    private boolean exChees = false;
+    private boolean exTopingVeg = false;
+    private boolean exTopingNonVeg = false;
+    private boolean bagData = false;
+
+    final int vegPizza = 300;
+    final int nonVegPizza = 400;
+    final int extraCheesePrice = 80;
+    final int exTopingsPriceVeg = 70;
+    final int exTopingsPriceNonVeg = 120;
+    final int bag = 20;
+
+    public Pizza(Boolean isVeg) {
+
         this.isVeg = isVeg;
-        // your code goes here
+        if (isVeg) {
+            this.price = 300;
+        } else {
+            this.price = 400;
+
+        }
+
     }
 
-    public int getPrice(){
+    public int getPrice() {
+
         return this.price;
     }
 
-    public void addExtraCheese(){
-        // your code goes here
+    public void addExtraCheese() {
+
+        if (!exChees) {
+            this.price = this.price + extraCheesePrice;
+            exChees = true;
+
+        }
+
     }
 
-    public void addExtraToppings(){
-        // your code goes here
+    public void addExtraToppings() {
+
+        if (!exTopingVeg && isVeg) {
+            this.price = this.price + exTopingsPriceVeg;
+            exTopingVeg = true;
+            exChees = true;
+
+        } else if (!exTopingNonVeg && !isVeg) {
+            this.price = this.price + exTopingsPriceNonVeg;
+            exTopingNonVeg = true;
+            exChees = true;
+
+        }
+
     }
 
-    public void addTakeaway(){
-        // your code goes here
+    public void addTakeaway() {
+
+        if (!bagData) {
+            bagData = true;
+            this.price = this.price + bag;
+        }
     }
 
-    public String getBill(){
-        // your code goes here
+    public String getBill() {
+
+        if (isVeg) {
+            this.bill = "Base Price Of The Pizza: " + vegPizza + "\n";
+        } else {
+            this.bill = "Base Price Of The Pizza: " + nonVegPizza + "\n";
+
+        }
+
+        if (exChees) {
+            this.bill = this.bill + "Extra Cheese Added: " + extraCheesePrice + "\n";
+        }
+
+        if (isVeg && exTopingVeg) {
+            this.bill = this.bill + "Extra Toppings Added: " + exTopingsPriceVeg + "\n";
+        } else if (!isVeg && exTopingNonVeg) {
+            this.bill = this.bill + "Extra Toppings Added: " + exTopingsPriceNonVeg + "\n";
+
+        }
+
+        if (bagData) {
+            this.bill = this.bill + "Paperbag Added: " + bag + "\n";
+        }
+
+        this.bill = this.bill + "Total Price: " + this.price + "\n";
+
         return this.bill;
     }
 }
